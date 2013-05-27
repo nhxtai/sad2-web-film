61f04b168e426606180558715ac2f426474bb824
tainhxse60639@gmail.com <tainhxse60639@gmail.com@e5911ed5-5354-dff2-e401-4912cd4a6c02>
2013-05-27 11:52:38 +0000

    Up Project

---
+++ trunk/Assignment/Assignment/Controllers/FilmController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class FilmController : Controller
+    {
+        //
+        // GET: /Film/
+
+        public ActionResult View(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ trunk/Assignment/Assignment/Controllers/HomePageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class HomePageController : Controller
+    {
+        //
+        // GET: /HomePage/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+    }
+}
