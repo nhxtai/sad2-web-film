762101e5c746be2ad221113e57344e3caeda7b24
tainhxse60639@gmail.com <tainhxse60639@gmail.com@e5911ed5-5354-dff2-e401-4912cd4a6c02>
2013-05-28 17:45:04 +0000

    Update Project : Create Some method in DAO class Re-Up Database/Assignment.bak

--- trunk/Source/Assignment/Assignment/Models/DAO/CategoryDAO.cs
+++ trunk/Source/Assignment/Assignment/Models/DAO/CategoryDAO.cs
@@ -7,5 +7,13 @@ namespace Assignment.Models.DAO
 {
     public class CategoryDAO
     {
+        /// <summary>
+        /// tạo danh sách tất cả thể loại phim
+        /// </summary>
+        public List<Category> GetAllCategory()
+        {
+            MoviesDataContext movie = new MoviesDataContext();
+            return movie.Categories.Where(c => c.IsActive == true).ToList();
+        }
     }
 }
