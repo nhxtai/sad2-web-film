91a1df732e205bebc7090f2c604082c857e4c7ab
tainhxse60639@gmail.com <tainhxse60639@gmail.com@e5911ed5-5354-dff2-e401-4912cd4a6c02>
2013-05-29 19:14:22 +0000

    Xoa Project

---
+++ Tool/AssignmentSAD/Assignment/Models/ViewModel/FilmDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class FilmDetailModel
+    {
+        public Film Film { get; set; }
+        public List<Comment> ListComment { get; set; }
+        public List<Category> ListCategory { get; set; }
+        public Account Account { get; set; }
+    }
+}
