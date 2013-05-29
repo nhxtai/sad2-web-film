ffd64e5c6fb90e3800d4c98b3d74f12dedac4ed4
tainhxse60639@gmail.com <tainhxse60639@gmail.com@e5911ed5-5354-dff2-e401-4912cd4a6c02>
2013-05-29 19:26:59 +0000

    Up lai Project

---
+++ Source/Assignment/Assignment/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment.Models.DAO;
+using Assignment.Models.ViewModel;
+namespace Assignment.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Category/
+
+        public ActionResult View(int id)
+        {
+            CategoryDAO cDao = new CategoryDAO();
+            FilmDAO fiDao = new FilmDAO();
+            var listCat = cDao.GetAllCategory();
+            CategoryModel cm = new CategoryModel()
+            {
+                ListCategory = listCat,
+                CurrentCat = cDao.GetACategory(id),
+                Account = null,
+                TopFilm = fiDao.GetTopFilm(5),
+                FilmOfThisCategory = fiDao.GetListFilmOfACategory(id),
+
+            };
+            return View(cm);
+        }
+
+    }
+}
---
+++ Source/Assignment/Assignment/Controllers/FilmController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment.Models;
+using Assignment.Models.DAO;
+namespace Assignment.Controllers
+{
+    public class FilmController : Controller
+    {
+        //
+        // GET: /Film/View/id
+
+        public ActionResult View(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Home", "HomePage");
+            }
+            int filmId = id ?? 1;
+            FilmDAO fiDao = new FilmDAO();
+            AccountDAO accDao = new AccountDAO();
+            CategoryDAO cDao = new CategoryDAO();
+            CommentDAO commentDao = new CommentDAO();
+            FilmDetailModel model = new FilmDetailModel
+            {
+                ListCategory = cDao.GetAllCategory(),
+                Account = null,
+                Film = fiDao.GetDetailAFilm(filmId),
+                ListComment = commentDao.GetAllCommentOfAFilm(filmId),
+                TopFilm = fiDao.GetTopFilm(5)
+            };
+            return View(model);
+        }
+
+    }
+}
