1bee58da6f2da58a6ba9764ad5e5e7094efe9ca3
maivantan1992@gmail.com <maivantan1992@gmail.com@e5911ed5-5354-dff2-e401-4912cd4a6c02>
2013-06-01 10:02:05 +0000

    Add register function, fix login

--- Source/Assignment/Assignment/Controllers/AccountController.cs
+++ Source/Assignment/Assignment/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Assignment.Models.DAO;
+using Assignment.Models.ViewModel;
+using Assignment.Models;
 
 namespace Assignment.Controllers
 {
@@ -39,6 +41,52 @@ namespace Assignment.Controllers
         {
             Session.Clear();
             return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            FilmDAO fiDao = new FilmDAO();
+            CategoryDAO cDao = new CategoryDAO();
+            RegisterModel register = new RegisterModel
+            {
+                Account = new Account(),
+                TopFilm = fiDao.GetTopFilm(5),
+                ListCategory = cDao.GetAllCategory(),
+            };
+            return View(register);
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(Account account, string Password_Retype)
+        {
+            AccountDAO accountDao = new AccountDAO();
+            if (Password_Retype.Equals(account.Password))
+            {
+                bool result = accountDao.AddNewAccount(account);
+                if (result == true)
+                {
+                    this.Session.Add("username", account.Username);
+                    this.Session.Add("role", account.Role);
+                    return RedirectToAction("Home", "HomePage");
+                }
+                TempData["register-error"] = "Tài khoản đã được sử dụng";
+            }
+            else
+            {
+                TempData["register-error"] = "Mật khẩu không giống nhau";
+            }
+            FilmDAO fiDao = new FilmDAO();
+            CategoryDAO cDao = new CategoryDAO();
+            RegisterModel register = new RegisterModel
+            {
+                Account = account,
+                TopFilm = fiDao.GetTopFilm(5),
+                ListCategory = cDao.GetAllCategory(),
+            };
+
+            return View(register);
         }
     }
 }
---
+++ Source/Assignment/Assignment/Models/ViewModel/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models.ViewModel
+{
+    public class RegisterModel
+    {
+        public List<Film> TopFilm { get; set; }
+        public Account Account { get; set; }
+        public List<Category> ListCategory { get; set; }
+    }
+}
